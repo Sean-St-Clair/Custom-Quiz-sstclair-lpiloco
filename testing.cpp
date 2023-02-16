f62f1749a77bf02fc659f5e58d862d76d4b1c8ad
Skeasty <lpiloco@uvm.edu>
2023-02-15 20:52:00 -0500

    testing.cpp complete

--- testing.cpp
+++ testing.cpp
@@ -1,7 +1,9 @@
 #include "Quiz.h"
 #include "Question.h"
 #include <iostream>
+
 using namespace std;
+
 int main() {
     bool passed = true;
     Question qn;
@@ -9,33 +11,39 @@ int main() {
     string prompt = qn.getPrompt();
     int points = qn.getPoints();
     if (prompt != "") {
-        cout << "FAILED constructor test: prompt" << endl;
+        cout << "FAILED Question constructor test: prompt" << endl;
+        passed = false;
     }
     if (points != 0) {
-        cout << "FAILED constructor test: points" << endl;
+        cout << "FAILED Question constructor test: points" << endl;
+        passed = false;
     }
 
     // setPrompt and getPrompt test
     qn.setPrompt("test prompt");
     if (qn.getPrompt() != "test prompt") {
         cout << "FAILED prompt test" << endl;
+        passed = false;
     }
 
     // setPoints and getPoints test
     qn.setPoints(77);
     if (qn.getPoints() != 77) {
         cout << "FAILED points test" << endl;
+        passed = false;
     }
 
     // getNumAnswers test
     if (qn.getNumAnswers() != 0) {
         cout << "FAILED getNumAnswers test" << endl;
+        passed = false;
     }
 
     // addAnswer by string and bool test
     qn.addAnswer("answer 1", 0);
     if (qn.getNumAnswers() != 1) {
         cout << "FAILED addAnswer by string and bool parameters test" << endl;
+        passed = false;
     }
 
     // addAnswer by answer object test
@@ -43,47 +51,96 @@ int main() {
     qn.addAnswer(a);
     if (qn.getNumAnswers() != 2) {
         cout << "FAILED addAnswer by answer struct parameter test" << endl;
+        passed = false;
     }
 
     // isCorrect by index test
     if (qn.isCorrect(0) != false && qn.isCorrect(1) != true) {
         cout << "FAILED isCorrect with index parameter test" << endl;
+        passed = false;
     }
 
     // isCorrect by string test
     if (qn.isCorrect("answer 1") != false && qn.isCorrect("answer 2") != true) {
         cout << "FAILED isCorrect with string parameter test" << endl;
+        passed = false;
     }
 
     // removeAnswer by index test
     qn.removeAnswer(0);
     if (qn.getNumAnswers() != 1) {
         cout << "FAILED removeAnswer by index test" << endl;
+        passed = false;
     }
 
     // removeAnswer by string test
+    qn.removeAnswer("answer 2");
+    if (qn.getNumAnswers() != 0) {
+        cout << "FAILED removeAnswer by index test" << endl;
+        passed = false;
+    }
 
     // clearAnswers test
+    qn.addAnswer(a);
+    qn.addAnswer("answer 1", 0);
+    qn.clearAnswers();
+    if (qn.getNumAnswers() != 0) {
+        cout << "FAILED clearAnswer test" << endl;
+        passed = false;
+    }
 
     Quiz qz;
     // Constructor test
+    if (qz.getTitle() != "" && qz.getTotalPointsCorrect() != 0 && qz.getTotalPointsPossible() != 0) {
+        cout << "FAILED quiz constructor test" << endl;
+        passed = false;
+    }
 
     // setTitle and getTitle test
+    qz.setTitle("test title");
+    if (qz.getTitle() != "test title") {
+        cout << "FAILED setTitle and getTitle test" << endl;
+        passed = false;
+    }
 
-    // addQuestion test
-
-    // GetNumberOfQuestions test
+    // addQuestion test and getNumberOfQuestions test
+    qz.addQuestion(qn);
+    if (qz.getNumberOfQuestions() != 1) {
+        cout << "FAILED addQuestion test" << endl;
+        passed = false;
+    }
 
     // GetQuestion by index test
+    optional<Question> testQn = qz.getQuestion(0);
+    if (testQn->getPrompt() != "test prompt") {
+        cout << "FAILED getQuestion by index test" << endl;
+        passed = false;
+    }
 
     // GetQuestion by string prompt test
+    optional<Question> testQn2 = qz.getQuestion("test prompt");
+    if (testQn2->getPrompt() != "test prompt") {
+        cout << "FAILED getQuestion by index test" << endl;
+        passed = false;
+    }
 
     // removeQuestion by index test
+    qz.removeQuestion(0);
+    if (qz.getNumberOfQuestions() != 0) {
+        cout << "FAILED removeQuestion by index test" << endl;
+        passed = false;
+    }
 
     // removeQuestion by string prompt test
+    qz.addQuestion(qn);
+    qz.removeQuestion("test prompt");
+    if (qz.getNumberOfQuestions() != 0) {
+        cout << "FAILED removeQuestion by index test" << endl;
+        passed = false;
+    }
 
     if (passed == true) {
         cout << "Passed all test cases" << endl;
     }
-    return passed;
+    return 0;
 }
